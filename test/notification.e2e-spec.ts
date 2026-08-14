55d551575bf6d2c62b7b6393f82de9f447afee41
Vann Soklay <soklayvann@gmail.com>
2026-08-14 19:44:38 +0700

    fix eslint

--- test/notification.e2e-spec.ts
+++ test/notification.e2e-spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return */
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { io, Socket } from 'socket.io-client';
